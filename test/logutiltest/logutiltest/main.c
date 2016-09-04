01410e566717536407490d74c28ccb2a3396a399
coryee <coryee2011@gmail.com>
2016-09-04 12:34:45 +0800

    update public methods

--- test/logutiltest/logutiltest/main.c
+++ test/logutiltest/logutiltest/main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-
 #include "logutil.h"
 
-#define CTMainLogLevel CTLogLevelFunc
-#define CTMainLog CTLogFunc
-CTLOGSETMODULE("main");
+#define CTMyLogInfoEx(fmt, ...) CTLogInfoEx("main", fmt, __VA_ARGS__)
+#define CTMyLogDebug CTLogDebug
+//CTLOGSETMODULE("main");
 //#define mylog(fmt, ...)	CTLogPrint(CTLOG_LEVEL_DEBUG, "main", fmt, __VA_ARGS__)
 #define mylog(fmt, ...)	printf(fmt, __VA_ARGS__)
 
 int main()
 {
-	CTLogInit(CTLOG_LEVEL_DEBUG, CTLOG_MODE_LOG2FILE, "coryee.log");
-	CTMainLog("%s\n", "hello");
-	CTMainLogLevel(CTLOG_LEVEL_DEBUG, "%s\n", "hello");
+	CTLogInit(CTLOG_LEVEL_DEBUG, CTLOG_MODE_LOG2CONSOLE, "coryee.log");
+	CTMyLogDebug("%s\n", "hello");
+	CTMyLogInfoEx("%s\n", "hello");
 
 	mylog("%s", "coryee");
 	return 0;
--- util/logutil.c
+++ util/logutil.c
@@ -1,16 +1,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+#include <windows.h> 
+#include <io.h>  
+#include <fcntl.h> 
+#else 
+#include <time.h>  
+#include <sys/time.h> 
+#endif
+
+#ifdef _WIN32
+#define snprintf sprintf_s
+#endif
+
 #include "logutil.h"
 
+struct CTLogContext {
+	CTLogLevel	level;
+	CTLogMode	mode;
+	FILE		*fpLog;
+	int			bInit;
+};
+
 #define CTLOG_FILE_NAME_DEFAULE	"default.log"
+
 static CTLogContext gLogContext;
 static char gLogBuffer[512];
 char CTLogLevelString[][16] = {
 	"DEBUG", 
-	"INFO"
+	"INFO",
+	"ERROR"
 };
 
+static void CTLogPrint(CTLogLevel level, char *pcModuleName, char *pcMessage);
+static void CTLogMessage(CTLogLevel level, char *pcModuleName, char *pcFormat, va_list ap);
+
+#ifdef _WIN32
+void CTLogOpenConsole()
+{
+	AllocConsole();
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	int hCrt = _open_osfhandle((long)handle, _O_TEXT);
+	FILE *hf = _fdopen(hCrt, "w");
+	*stdout = *hf;
+}
+#endif
 
 int CTLogInit(CTLogLevel level, CTLogMode mode, char *pcFileName)
 {
@@ -19,7 +54,7 @@ int CTLogInit(CTLogLevel level, CTLogMode mode, char *pcFileName)
 	gLogContext.level = level;
 	gLogContext.mode = mode;
 
-	if (gLogContext.mode | CTLOG_MODE_LOG2FILE)
+	if (gLogContext.mode == CTLOG_MODE_LOG2FILE)
 	{
 		// close file handle if LogContext has been initialized
 		if (gLogContext.fpLog != NULL)
@@ -31,48 +66,131 @@ int CTLogInit(CTLogLevel level, CTLogMode mode, char *pcFileName)
 		if (gLogContext.fpLog == NULL)
 			return CTLOG_EC_FAILURE;
 	}
+	else
+	{
+#ifdef _WIN32
+		CTLogOpenConsole();
+#endif // _WIN32
+		gLogContext.fpLog = stdout;
+	}
 
 	return CTLOG_EC_OK;
 }
 
-char *CTLogGetFormatString(char *pcFormat, va_list al)
+int CTLogDeInit()
 {
-	if (-1 == _vsnprintf(gLogBuffer, sizeof(gLogBuffer), pcFormat, al))
-		return "";
-	return gLogBuffer;
+	if (gLogContext.mode == CTLOG_MODE_LOG2FILE)
+	{
+		// close file handle if LogContext has been initialized
+		if (gLogContext.fpLog != NULL)
+			fclose(gLogContext.fpLog);
+	}
+
+	return CTLOG_EC_OK;
 }
 
-int CTLogPrintInternal(char *pcFormat, ...)
+void CTLogDebugEx(char *pcModuleName, char *pcFormat, ...)
 {
-	va_list al;
-	char *pcFmtStr;
+	va_list ap;
+	va_start(ap, pcFormat);
+	CTLogMessage(CTLOG_LEVEL_DEBUG, pcModuleName, pcFormat, ap);
+	va_end(ap);
+}
 
+void CTLogDebug(char *pcFormat, ...)
+{
+	va_list ap;
+	va_start(ap, pcFormat);
+	CTLogMessage(CTLOG_LEVEL_DEBUG, NULL, pcFormat, ap);
+	va_end(ap);
+}
+
+void CTLogInfoEx(char *pcModuleName, char *pcFormat, ...)
+{
+	va_list ap;
+	va_start(ap, pcFormat);
+	CTLogMessage(CTLOG_LEVEL_INFO, pcModuleName, pcFormat, ap);
+	va_end(ap);
+}
+
+void CTLogInfo(char *pcFormat, ...)
+{
+	va_list ap;
+	va_start(ap, pcFormat);
+	CTLogMessage(CTLOG_LEVEL_INFO, NULL, pcFormat, ap);
+	va_end(ap);
+}
+
+void CTLogErrorEx(char *pcModuleName, char *pcFormat, ...)
+{
+	va_list ap;
+	va_start(ap, pcFormat);
+	CTLogMessage(CTLOG_LEVEL_ERROR, pcModuleName, pcFormat, ap);
+	va_end(ap);
+}
+
+void CTLogError(char *pcFormat, ...)
+{
+	va_list ap;
+	va_start(ap, pcFormat);
+	CTLogMessage(CTLOG_LEVEL_ERROR, NULL, pcFormat, ap);
+	va_end(ap);
+}
+
+
+// private methods
+static void CTLogGetCurrentTime(char *pcCurTime, int iSize)
+{
+#ifdef _WIN32
+	SYSTEMTIME sys;
+	
+	pcCurTime[0] = 0;
+	GetLocalTime(&sys);
+	snprintf(pcCurTime, iSize, "%4d-%02d-%02d %02d:%02d:%02d.%03d", sys.wYear, sys.wMonth, sys.wDay, sys.wHour, sys.wMinute, sys.wSecond, sys.wMilliseconds);
+#else 
+	struct timeval		tv;
+	struct timezone		tz;
+	struct tm			*p;
+
+	pcCurTime[0] = 0;
+	gettimeofday(&tv, &tz);
+	p = localtime(&tv.tv_sec);
+	snprintf(pcCurTime, iSize, "%4d-%02d-%02d %02d:%02d:%02d.%03d", 
+		1900 + p->tm_year,
+		1 + p->tm_mon, 
+		p->tm_mday, 
+		p->tm_hour, 
+		p->tm_min, 
+		p->tm_sec, 
+		tv.tv_usec);
+#endif
+}
+
+static void CTLogPrint(CTLogLevel level, char *pcModuleName, char *pcMessage)
+{
+	CTLogContext *pLogCtx = &gLogContext;
+	char pcCurTime[32];
+
+	CTLogGetCurrentTime(pcCurTime, 32);
 	//TODO: need to use mutex to protect
-
-	va_start(al, pcFormat);
-	pcFmtStr = CTLogGetFormatString(pcFormat, al);
-	va_end(al);
-	printf(pcFmtStr);
-
-	return CTLOG_EC_OK;
+	if (pcModuleName == NULL || pcModuleName[0] == 0)
+		fprintf(pLogCtx->fpLog, "%s [%s] %s\r\n", pcCurTime, CTLogLevelString[level], pcMessage);
+	else
+		fprintf(pLogCtx->fpLog, "%s [%s] <%s> %s\r\n", pcCurTime, CTLogLevelString[level], pcModuleName, pcMessage);
+	fflush(pLogCtx->fpLog);
 }
 
-
-int CTLogPrint(CTLogLevel level, char *pcModuleName, char *pcFormat, ...)
+static void CTLogMessage(CTLogLevel level, char *pcModuleName, char *pcFormat, va_list ap)
 {
-	va_list al;
-	char *pcFmtStr;
+	if (level < 0 || level >= CTLOG_NUM_LOG_LEVELS)
+		return;
 
 	if (level < gLogContext.level)
-		return CTLOG_EC_OK;
+		return;
 
-	CTLogPrintInternal("[%s] [%s] ", CTLogLevelString[level], pcModuleName);
+	_vsnprintf(gLogBuffer, sizeof(gLogBuffer), pcFormat, ap);
+	if (gLogBuffer[strlen(gLogBuffer) - 1] == '\n')
+		gLogBuffer[strlen(gLogBuffer) - 1] = 0;
 
-	va_start(al, pcFormat);
-	pcFmtStr = CTLogGetFormatString(pcFormat, al);
-	va_end(al);
-
-	CTLogPrintInternal("%s", pcFmtStr);
-
-	return CTLOG_EC_OK;
+	CTLogPrint(level, pcModuleName, gLogBuffer);
 }
